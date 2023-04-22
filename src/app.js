31b27183fbeb18b30405e93e1457a70470106efc
enzocosson <enzolemercier@gmail.com>
2023-04-23 00:51:40 +0200

    loader logo colors change without gltf

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  loader.classList.add("loader__active");
+});
+
 const frontButton = document.getElementById("front");
 const backButton = document.getElementById("back");
 const leftButton = document.getElementById("left");
